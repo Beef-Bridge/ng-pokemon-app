623bcb78ffacc3d0e8987554f932264d6700e68c
Nikoquattro <nikoquattro@gmail.com>
2022-10-08 22:59:02 +0200

    utilisation de la classe de mock et de l'entité pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{ pokemonList[0] }}!</h1>`
+  template: `<h1>Liste de pokémons</h1>`
 })
 export class AppComponent implements OnInit {
-  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce '];
+  pokemonList: Pokemon[] = POKEMONS;
 
   constructor() {
     //this.pokemonList = []; 
@@ -15,11 +17,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     console.table(this.pokemonList);
-    this.selectPokemon('Bulbizarre');
+    this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemonName: string) {
-    console.log(`On a cliqué sur le pokémon ${pokemonName}`);
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`On a cliqué sur le pokémon ${pokemon.name}`);
   }
 }
  
