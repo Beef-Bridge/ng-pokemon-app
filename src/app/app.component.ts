80a8298a6ddd3962142013155ed34aa1c92e51e8
Nikoquattro <nikoquattro@gmail.com>
2022-10-09 22:43:44 +0200

    Modifier le composant de la liste des pokémons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
-
-  constructor() {
-    //this.pokemonList = []; 
-    // -> Ne pas déclaré une propriete de cette facon !
-    // -> Une bonne pratique est de garder la logique du composant en dehors du constructor !
-  }
-
-  ngOnInit(): void {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string) {
-    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
-    if (pokemon) {
-      console.log(`On a demandé le pokémon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`On a demandé un pokémon qui n'existe pas`);
-      this.pokemonSelected = pokemon;      
-    }
-  }
+export class AppComponent {
 }
- 
