8d9e1fe3f5d06bcf2fda03ac08f571ad06960e8c
Nikoquattro <nikoquattro@gmail.com>
2022-11-01 17:47:59 +0100

    fournir le pokemon.service au niveau d'un module eet non plus au niveau de l'application générale

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ListPokemonComponent } from "./list-pokemon/list-pokemon.component";
+import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.component";
+import { BorderCardDirective } from "./border-card.directive";
+import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
+import { RouterModule, Routes } from "@angular/router";
+import { PokemonService } from "./pokemon.service";
 
 const pokemonRoutes: Routes = [
-  { path: 'pokemons', component: ListPokemonComponent },
-  { path: 'pokemon/:id', component: DetailPokemonComponent }
+  { path: "pokemons", component: ListPokemonComponent },
+  { path: "pokemon/:id", component: DetailPokemonComponent },
 ];
 
 @NgModule({
@@ -16,11 +17,9 @@ const pokemonRoutes: Routes = [
     ListPokemonComponent,
     DetailPokemonComponent,
     BorderCardDirective,
-    PokemonTypeColorPipe
+    PokemonTypeColorPipe,
   ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(pokemonRoutes)
-  ]
+  imports: [CommonModule, RouterModule.forChild(pokemonRoutes)],
+  providers: [PokemonService],
 })
-export class PokemonModule { }
+export class PokemonModule {}
