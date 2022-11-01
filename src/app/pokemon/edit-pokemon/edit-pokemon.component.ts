0f22ffd8cc671fa32dd38d33bc468dee3cc16dd5
Nikoquattro <nikoquattro@gmail.com>
2022-11-01 21:59:02 +0100

    center le titre de la page du formulaire d'édition

--- src/app/pokemon/edit-pokemon/edit-pokemon.component.ts
+++ src/app/pokemon/edit-pokemon/edit-pokemon.component.ts
@@ -5,7 +5,7 @@ import { PokemonService } from "../pokemon.service";
 
 @Component({
   selector: "app-edit-pokemon",
-  template: `<h2>Editer {{ pokemon?.name }}</h2>
+  template: `<h2 class="center">Editer {{ pokemon?.name }}</h2>
     <p *ngIf="pokemon" class="center"><img [src]="pokemon.picture" /></p>
     <app-pokemon-form *ngIf="pokemon" [pokemon]="pokemon"></app-pokemon-form>`,
   styles: [],
