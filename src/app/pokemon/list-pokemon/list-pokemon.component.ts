b3d71ea23ad1e44bfd432a2700d908faae309301
Nikoquattro <nikoquattro@gmail.com>
2022-11-01 17:41:35 +0100

    consommation du pokemon.service.ts

--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -1,18 +1,22 @@
-import { Component  } from '@angular/core';
-import { Router } from '@angular/router';
-import { POKEMONS } from '../mock-pokemon-list';
-import { Pokemon } from '../pokemon';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Pokemon } from "../pokemon";
+import { PokemonService } from "../pokemon.service";
 
 @Component({
-  selector: 'app-list-pokemon',
-  templateUrl: './list-pokemon.component.html'
+  selector: "app-list-pokemon",
+  templateUrl: "./list-pokemon.component.html",
 })
-export class ListPokemonComponent {
-  pokemonList: Pokemon[] = POKEMONS;
+export class ListPokemonComponent implements OnInit {
+  pokemonList: Pokemon[];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private pokemonSerivce: PokemonService) {}
+
+  ngOnInit() {
+    this.pokemonList = this.pokemonSerivce.getPokemonList();
+  }
 
   goToPokemon(pokemon: Pokemon) {
-    this.router.navigate(['/pokemon', pokemon.id]);
+    this.router.navigate(["/pokemon", pokemon.id]);
   }
 }
