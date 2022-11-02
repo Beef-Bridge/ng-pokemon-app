b46dc468a51ad2b63e89880f9bc663530cee670d
Nikoquattro <nikoquattro@gmail.com>
2022-11-02 23:16:35 +0100

    Consommer des données asynchrones

--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -13,7 +13,9 @@ export class ListPokemonComponent implements OnInit {
   constructor(private router: Router, private pokemonSerivce: PokemonService) {}
 
   ngOnInit() {
-    this.pokemonList = this.pokemonSerivce.getPokemonList();
+    this.pokemonSerivce
+      .getPokemonList()
+      .subscribe((pokemonList) => (this.pokemonList = pokemonList));
   }
 
   goToPokemon(pokemon: Pokemon) {
