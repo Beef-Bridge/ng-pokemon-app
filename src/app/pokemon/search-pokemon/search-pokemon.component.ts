e63738d95a7d55098255582bccf6b5d0f20703cc
Nikoquattro <nikoquattro@gmail.com>
2022-11-13 23:12:27 +0100

    construction un observable personnalisé

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { Observable, Subject } from "rxjs";
+import {
+  debounceTime,
+  distinctUntilChanged,
+  Observable,
+  Subject,
+  switchMap,
+} from "rxjs";
 import { Pokemon } from "../pokemon";
+import { PokemonService } from "../pokemon.service";
 
 @Component({
   selector: "app-search-pokemon",
@@ -11,9 +18,15 @@ export class SearchPokemonComponent implements OnInit {
   searchTerms = new Subject<string>();
   pokemons$: Observable<Pokemon[]>;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private pokemonService: PokemonService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.pokemons$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term) => this.pokemonService.searchPokemonList(term))
+    );
+  }
 
   search(term: string) {
     this.searchTerms.next(term);
