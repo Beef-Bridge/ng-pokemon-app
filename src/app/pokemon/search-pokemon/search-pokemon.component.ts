e38c29e476cf45f74cf5c01da1cbce28289660a8
Nikoquattro <nikoquattro@gmail.com>
2022-11-13 18:49:07 +0100

    construire un composant de recherche (ng generate component pokemon/search-pokemon --inline-template=false)

---
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Observable, Subject } from "rxjs";
+import { Pokemon } from "../pokemon";
+
+@Component({
+  selector: "app-search-pokemon",
+  templateUrl: "./search-pokemon.component.html",
+})
+export class SearchPokemonComponent implements OnInit {
+  searchTerms = new Subject<string>();
+  pokemons$: Observable<Pokemon[]>;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  search(term: string) {
+    this.searchTerms.next(term);
+  }
+
+  goToDetail(pokemon: Pokemon) {
+    const link = ["/pokemon", pokemon.id];
+    this.router.navigate(link);
+  }
+}
